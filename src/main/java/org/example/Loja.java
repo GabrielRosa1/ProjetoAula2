10bd6cbabd8dff95f165f5fc3fea389b80fa6741
GabrielRosa1 <gabrielrosamart@gmail.com>
2026-03-03 16:18:43 -0300

    feat(classe main/Loja fazendo a chamada de todos os métodos e printando as respostas)

--- src/main/java/org/example/Loja.java
+++ src/main/java/org/example/Loja.java
@@ -4,12 +4,42 @@ import java.util.ArrayList;
 
 public class Loja {
     static void main() {
+
         ArrayList<Venda> vendas = Arquivo.abrirArquivo("vendas.csv");
-        double faturamentoTotal = Venda.faturamentoTotal(vendas);
-        int quantidadeVendas = Venda.quantidadeVendas(vendas);
-        double valorMedioVenda = Venda.valorMedioVenda(vendas);
-        System.out.println(faturamentoTotal);
-        System.out.println(quantidadeVendas);
-        System.out.println(valorMedioVenda);
+
+        System.out.printf("%n");
+        System.out.println("===============Cálculos  Gerais===============");
+        System.out.printf("%n");
+        System.out.printf("Faturamento total da loja: %.2f$%n", Venda.faturamentoTotal(vendas));
+        System.out.printf("Quantidade total de itens vendidos: %s%n", Venda.quantidadeVendas(vendas));
+        System.out.printf("Valor médio por item vendido: %.2f$%n", Venda.valorMedioVenda(vendas));
+        System.out.printf("%n");
+        System.out.println("===========Estatísticas por Cliente===========");
+        System.out.printf("%n");
+        System.out.println("=======Melhor  Cliente=======");
+        System.out.printf("Melhor Cliente (mais gastou) %n Nome: %s %n Total Gasto: %.2f$ %n Quantidade de Compras: %s %n",
+                Cliente.clienteMaisGastou(vendas).getNome(), Cliente.clienteMaisGastou(vendas).getTotalGasto(), Cliente.clienteMaisGastou(vendas).getQuantidadeCompras());
+        System.out.println("=============================");
+        System.out.printf("%n");
+        System.out.println("======Total por Cliente======");
+        Cliente.totalPorCliente(vendas);
+        System.out.printf("%n");
+        System.out.println("===========Estatísticas por Produto===========");
+        System.out.printf("%n");
+        System.out.println("=========Produto Mais Vendido==========");
+        System.out.printf("Nome: %s %n Total Vendido: %s %n Faturamento Total: %.2f$ %n",
+            Produto.maisVendido(vendas).getNome(), Produto.maisVendido(vendas).getTotalVendido(), Produto.maisVendido(vendas).getFaturamento());
+        System.out.println("=======================================");
+        System.out.printf("%n");
+        System.out.println("=======Produto Maior Faturamento=======");
+        System.out.printf("Nome: %s %n Total Vendido: %s %n Faturamento Total: %.2f$ %n",
+                Produto.maiorFaturamento(vendas).getNome(), Produto.maiorFaturamento(vendas).getTotalVendido(), Produto.maiorFaturamento(vendas).getFaturamento());
+        System.out.println("=======================================");
+        System.out.printf("%n");
+        System.out.println("===========Total por Produto===========");
+        Produto.totalPorProduto(vendas);
+        System.out.printf("%n");
+        System.out.println("==============================================");
+
     }
 }
